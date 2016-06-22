9380da49b77ac624c0d84c4e05cfe76c91b1d293
Chase McElroy <ichasem@gmail.com>
2016-06-22 15:14:37 -0700

    Added game vc and card xib

---
+++ BrainTeaser/GameVC.swift
@@ -0,0 +1,26 @@
+//
+//  GameVC.swift
+//  BrainTeaser
+//
+//  Created by Chase McElroy on 6/22/16.
+//  Copyright © 2016 Chase McElroy. All rights reserved.
+//
+
+import UIKit
+
+class GameVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
